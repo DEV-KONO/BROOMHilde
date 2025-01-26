27ddfe08f8f209c47f0a0a1c5dd43b1793502cf6
risc99 <rickyissac99@gmail.com>
2025-01-26 12:34:12 -0600

    Cosas

--- 2Level/cuevaSCRIPT.gd
+++ 2Level/cuevaSCRIPT.gd
@@ -1,10 +1,9 @@
 extends Node2D
 
 
-#var obstacle = preload("res://obstacle_nodes/tree.tscn")
-#var items = preload("res://Item/items.tscn")
-#var item_sprites = [preload("res://Item/Item_Sprites/Books.png"), preload("res://Item/Item_Sprites/CalacaChida.png"), preload("res://Item/Item_Sprites/CrystalBall.png"), preload("res://Item/Item_Sprites/Crystals.png")]
-@onready var camara: Camera2D = $Camera2D
+var estalagmita = preload("res://2Level/obstaculos/collision_4.tscn")
+var estalactita = preload("res://2Level/obstaculos/collision_3.tscn")
+@onready var camara: Camera2D = $camLvl2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,18 +15,16 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_obstacle_spawn_timer_timeout() -> void:
-	#var tree = obstacle.instantiate()
-	#var item = items.instantiate()
-	#var item_sprite = item_sprites[randi() % item_sprites.size()]
-	#item.get_node("Sprite2D").texture = item_sprite
-	#tree.position = camara.get_cam_pos() + Vector2(1200, 540)
-	#item.position = camara.get_cam_pos() + Vector2(1000, randf_range(-530,87))
-	#add_child(tree)
-	#add_child(item)
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
 	pass
 
-
-func _on_button_2_pressed() -> void:
-	#get_tree().quit()
+func _on_timer_timeout() -> void:
+	var estalactitas = estalactita.instantiate()
+	var estalagmitas = estalagmita.instantiate()
+	#var item_sprite = item_sprites[randi() % item_sprites.size()]
+	estalactitas.position = camara.get_cam_pos() + Vector2(randi_range(900,1200), 4)
+	estalagmitas.position = camara.get_cam_pos() + Vector2(randi_range(900,1200), 1086)
+	add_child(estalactitas)
+	add_child(estalagmitas)
 	pass
